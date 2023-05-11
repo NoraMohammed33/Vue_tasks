ad9d53d16d07b6c38a9c47675b95f1157581ec5a
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-05-11 05:20:48 +0300

    first demo

---
+++ demo/product.js
@@ -0,0 +1,49 @@
+products = [
+    {
+        id: 100,
+        name: 'product one',
+        price: 1000,
+        instock: 5,
+        description: "product one is  simply text product one is  simply textproduct one is  simply text product one is  simply text product one is  simply text",
+        image:"imgs/4.jpeg"
+    },
+    {
+        id: 101,
+        name: 'product two',
+        price: 4000,
+        instock: 3,
+        description: "product one is  simply text product one is  simply text product one is  simply text product one is  simply text product one is  simply text",
+        image: "imgs/2.jpg"
+    },
+    {
+        id: 102,
+        name: 'product three',
+        price: 3000,
+        instock: 11,
+        description: "product one is  simply textproduct one is  simply textproduct one is  simply text",
+        image: "imgs/3.jpg"
+    },
+    {
+        id: 102,
+        name: 'product three',
+        price: 2000,
+        instock: 0,
+        description: "product one is  simply text product one is  simply text product one is  simply text",
+        image: "imgs/1.jpg"
+    }, {
+        id: 102,
+        name: 'product three',
+        price: 3000,
+        instock: 7,
+        description: "product one is  simply text product one is  simply text product one is  simply text",
+        image: "imgs/3.jpg"
+    },
+    {
+        id: 102,
+        name: 'product three',
+        price: 4000,
+        instock: 5,
+        description: "product one is  simply text product one is  simply text product one is  simply text",
+        image: "imgs/2.jpg"
+    }
+]
