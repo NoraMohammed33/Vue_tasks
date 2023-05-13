f377ce58e8a42f7dc0abd03376888705920cbb4e
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-05-13 18:06:59 +0300

    add task1

---
+++ task1/book.js
@@ -0,0 +1,68 @@
+books = [
+    {
+        id: 100,
+        name: 'Book one',
+        price: 1000,
+        pages: 45,
+        author: "Noor",
+        isbn: 30,
+        category: "category1",
+        image: "imgs/4.jpeg",
+        instock:3
+    },
+    {
+        id: 101,
+        name: 'Book two',
+        price: 100,
+        pages: 3,
+        author: "Nora",
+        isbn:90,
+        category:"category2",
+        image: "imgs/2.jpg",
+        instock:5
+    },
+    {
+        id: 102,
+        name: 'Book three',
+        price: 3000,
+        pages: 50,
+        author: "walla",
+        isbn:70,
+        category: "category3",
+        image: "imgs/3.jpg",
+        instock:10
+
+    },
+    {
+        id: 102,
+        name: 'Book three',
+        price: 2000,
+        pages: 0,
+        isbn: 200,
+        author: "Noor",
+        category: "category4",
+        image: "imgs/1.jpg",
+        instock:8
+    }, {
+        id: 102,
+        name: 'Book three',
+        price: 3000,
+        pages: 40,
+        author: "Nooooor",
+        isbn:35,
+        category: "category5",
+        image: "imgs/3.jpg",
+        instock:15
+    },
+    {
+        id: 102,
+        name: 'Book three',
+        price: 4000,
+        pages: 60,
+        author:"Noha",
+        isbn:80,
+        category: "category6",
+        image: "imgs/2.jpg",
+        instock:0
+    }
+]
